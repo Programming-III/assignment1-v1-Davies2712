b079951539314dc429a103c2d0b8b714d6d31b83
DavidRamy <davidramy0890@gmail.com>
2025-11-12 15:28:48 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,14 +1,25 @@
-#ifndef BIRD_H
-#define BIRD_H
+//
+// Created by David on 12/11/2025.
+//
+
+#ifndef MILESTONE_1_BIRD_H
+#define MILESTONE_1_BIRD_H
+#include <iostream>
 
 #include "Animal.h"
+using namespace std;
 
-//define class bird here 
+class Bird : public Animal {
+private:
+    float wingSpan;
+    public:
+    Bird();
+    Bird(string n, int a, bool h,float ws) : Animal(n,a,h) {
+        wingSpan=ws;
+    }
+    ~Bird();
+    void setWingSpan(float ws);
+    float getWingSpan();
 
-
-
-
-
-
-
-#endif
+};
+#endif //MILESTONE_1_BIRD_H
