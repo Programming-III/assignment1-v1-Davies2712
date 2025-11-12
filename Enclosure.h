a7e8e6da7d097716b29f48a18663c12c8079b438
DavidRamy <davidramy0890@gmail.com>
2025-11-12 15:29:21 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,39 @@
-#ifndef ENCLOSURE_H
-#define ENCLOSURE_H
+//
+// Created by David on 12/11/2025.
+//
+
+#ifndef MILESTONE_1_ENCLOSURE_H
+#define MILESTONE_1_ENCLOSURE_H
+#include <iostream>
+using namespace std;
+
+#include <string>
 
 #include "Animal.h"
 
-//define enclosure class here 
+class Enclosure : public Animal{
+private:
+    Animal* animal;
+    int capacity;
+    int currentCount;
+    public:
+    Enclosure();
+    Enclosure(string n, int a, bool h,Animal* a) : Animal(n,a,h) {
+        animal=new Animal[];
+    }
+
+    ~Enclosure() {
+        delete []animal;
+    }
+
+    void setCapacity(int c);
+    void setCurrentCount(int c);
+    Animal* getAnimal();
+    int getCapacity();
+    int getCurrentCount();
+    void setAnimal(Animal a);
 
 
 
-
-
-
-
-
-#endif
+};
+#endif //MILESTONE_1_ENCLOSURE_H
