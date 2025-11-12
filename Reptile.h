08aa803d4bf2406268b4acc363e64dbea5e2831d
DavidRamy <davidramy0890@gmail.com>
2025-11-12 15:34:46 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,30 @@
-#ifndef REPTILE_H
-#define REPTILE_H
+//
+// Created by David on 12/11/2025.
+//
+
+#ifndef MILESTONE_1_REPTILE_H
+#define MILESTONE_1_REPTILE_H
+#include <iostream>
+using namespace std;
 
 #include "Animal.h"
 
-//define reptile class here
+class Reptile : public Animal {
+private:
+    bool isVenomous;
+    public:
+    Reptile();
+    Reptile(string n, int a, bool h,bool v) : Animal(n,a,h) {
+        isVenomous=v;
+    }
+
+    ~Reptile();
+    void setVenomous(bool v);
+    bool getVenomous();
 
 
 
 
 
-
-
-
-
-#endif
+};
+#endif //MILESTONE_1_REPTILE_H
