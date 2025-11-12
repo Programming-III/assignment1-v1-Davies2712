5579b982e3fa6f75cba46553615dc566cc328bf4
DavidRamy <davidramy0890@gmail.com>
2025-11-12 15:29:51 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,29 @@
-#ifndef MAMMAL_H
-#define MAMMAL_H
+//
+// Created by David on 12/11/2025.
+//
+
+#ifndef MILESTONE_1_MAMMAL_H
+#define MILESTONE_1_MAMMAL_H
+#incloud <iostream>
+#include <string>
 
 #include "Animal.h"
 
-//define mammal class here
+using namespace std;
+class Mammal : public Animal {
+    private:
+    string furColor;
+    public:
+    Mammal(){};//error expected
+    Mammal(string n, int a, bool h,string f) : Animal(n,a,h) {
+        furColor = f;
+    }
+    ~Mammal(){};
+
+    void setFurColor(string fc);
+    string getFurColor();
 
 
+};
 
-
-
-
-#endif
+#endif //MILESTONE_1_MAMMAL_H
